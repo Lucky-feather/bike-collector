d7fb662c6f4401b50024213011253438012ef836
LH <lindsaynhenderson@gmail.com>
2022-11-04 16:43:53 -0500

    build landing page

--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,22 @@ def home(request):
 # Create your views here.
 def about(request):
   return render(request, 'about.html')
+
+class Bike:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, type, description, pros, cons):
+    self.type = type
+    self.description = description
+    self.pros = pros
+    self.cons = cons
+
+bikes = [
+  Bike('Gravel Bike', 'Thicker tires than a road bike but not built for mountain trails.', 'durable', 'slower than a regular road bike' ),
+  Bike('EBike', 'bike with a motor that helps add power to pedaling', 'makes hills easier', 'less exercise' )
+]
+
+def bikes_index(request):
+  return render(request, 'bikes/index.html', {'bikes': bikes })
+
+def home(request):
+  return render(request, 'home.html')
+
