06bc7444e9941c13c4b0c4a0cf88e588b285fd54
LH <lindsaynhenderson@gmail.com>
2022-11-04 19:40:24 -0500

    adjust model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Bike(models.Model):
+  type = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  pros = models.TextField(max_length=250)
+  cons = models.TextField(max_length=250)
+
+  def __str__(self):
+    return self.type
+''
