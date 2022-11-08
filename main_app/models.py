5ccd79545a180dbc7bb31c12a81e6fb007210eb6
LH <lindsaynhenderson@gmail.com>
2022-11-07 20:26:59 -0600

    build maintenance model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+WORK = (
+  ('T', 'Air Up Tires'),
+  ('G', 'Grease Chain'),
+  ('C', 'Check Brakes')
+)
 class Bike(models.Model):
   type = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
@@ -12,3 +17,15 @@ class Bike(models.Model):
   
   def get_absolute_url(self):
     return reverse('bikes_detail', kwargs={'bike_id': self.id})
+
+class Maintenance(models.Model):
+  date = models.DateField()
+  work = models.CharField(
+    max_length=1,
+    choices=WORK,
+    default=WORK[0][0]
+  )
+  bike = models.ForeignKey(Bike, on_delete=models.CASCADE)
+  
+  def __str__(self): 
+    return f"{self.get_work_display()} on {self.date}"
