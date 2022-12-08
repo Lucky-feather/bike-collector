cd93c8072c06cc522468fae5bf0c486215ae3eef
LH <lindsaynhenderson@gmail.com>
2022-12-08 15:55:16 -0600

    allow hosts

--- bikecollector/settings.py
+++ bikecollector/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
+
 
 # Application definition
 
