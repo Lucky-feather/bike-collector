e682fd1c17585eb4ac9eedc039a555cd2bab3dea
LH <lindsaynhenderson@gmail.com>
2022-12-08 18:48:00 -0600

    change allowed hosts

--- bikecollector/settings.py
+++ bikecollector/settings.py
@@ -16,6 +16,7 @@ environ.Env.read_env()
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
