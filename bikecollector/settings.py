6c72302fce3e1577b220321a2ee5d3d5247b6494
LindsayH <lindsaynhenderson@gmail.com>
2022-12-14 11:34:45 -0600

    remove trusted origins

--- bikecollector/settings.py
+++ bikecollector/settings.py
@@ -133,7 +133,7 @@ LOGIN_REDIRECT_URL = 'bikes_index'
 
 LOGOUT_REDIRECT_URL = 'home'
 
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
+# CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
